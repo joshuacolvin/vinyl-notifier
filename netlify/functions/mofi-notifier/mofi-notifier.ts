514713653ff7ca37f70e852748407af0ddc7f0df
Joshua Colvin <joshua@joshuacolvin.net>
2023-01-15 18:55:00 -0500

    feat: move endpoint to env

--- netlify/functions/mofi-notifier/mofi-notifier.ts
+++ netlify/functions/mofi-notifier/mofi-notifier.ts
@@ -2,13 +2,14 @@ import { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
 import cheerio from "cheerio";
 import fetch from "../../../src/utils/fetch";
 
-const API_ENDPOINT = "https://mofi.com/collections/back-in-stock/vinyl";
+const ENDPOINT =
+  process.env?.MOFI_ENDPOINT || "https://mofi.com/collections/back-in-stock";
 
 export const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
-  const body = await fetch(API_ENDPOINT);
+  const body = await fetch(ENDPOINT);
 
   return {
     statusCode: 200,
