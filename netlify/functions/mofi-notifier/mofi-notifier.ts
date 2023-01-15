2f16285dfc8aca76b340d7fcfe9db48438c2c9c5
Joshua Colvin <joshua@joshuacolvin.net>
2023-01-15 18:33:20 -0500

    feat: fetch data

--- netlify/functions/mofi-notifier/mofi-notifier.ts
+++ netlify/functions/mofi-notifier/mofi-notifier.ts
@@ -1,15 +1,20 @@
 import { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
+import cheerio from "cheerio";
+import fetch from "node-fetch";
 
 export const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
-  const name = event.queryStringParameters?.name || "Stranger";
+  const response = await fetch(
+    "https://mofi.com/collections/back-in-stock/vinyl"
+  );
+  const body = await response.text();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: body,
     }),
   };
 };
