b2af974e1e37cd40a66356c8fac92c47010d7385
Joshua Colvin <joshua@joshuacolvin.net>
2023-01-15 19:51:12 -0500

    feat: schedule function

--- netlify/functions/mofi-notifier/mofi-notifier.ts
+++ netlify/functions/mofi-notifier/mofi-notifier.ts
@@ -1,21 +1,28 @@
-import { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
+import {
+  Handler,
+  HandlerContext,
+  HandlerEvent,
+  schedule,
+} from "@netlify/functions";
 import { mofiScraper } from "../../../src/scrapers/mofi-scraper";
 import fetch from "../../../src/utils/fetch";
 
 const ENDPOINT =
   process.env?.MOFI_ENDPOINT || "https://mofi.com/collections/back-in-stock";
 
-export const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext
-) => {
-  const body = await fetch(ENDPOINT);
-  const result = await mofiScraper(body);
+const CRON = process.env?.CRON || "* 12 * * *";
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: result,
-    }),
-  };
-};
+export const handler: Handler = schedule(
+  CRON,
+  async (event: HandlerEvent, context: HandlerContext) => {
+    const body = await fetch(ENDPOINT);
+    const result = await mofiScraper(body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: result,
+      }),
+    };
+  }
+);
