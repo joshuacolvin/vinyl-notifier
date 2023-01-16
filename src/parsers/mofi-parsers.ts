7dd5a19b755ca1fc3c1aee7b15e47563bd4019df
Joshua Colvin <joshua@joshuacolvin.net>
2023-01-15 20:33:35 -0500

    feat: send url

--- src/parsers/mofi-parsers.ts
+++ src/parsers/mofi-parsers.ts
@@ -1,8 +1,11 @@
 import { Cheerio, CheerioAPI, Element } from "cheerio";
 import { MOFI_FILTERS } from "../constants/mofi-constants";
 
+const ENDPOINT =
+  process.env?.MOFI_ENDPOINT || "https://mofi.com/collections/back-in-stock";
+
 export function mofiParser(elements: Cheerio<Element>, $: CheerioAPI): string {
-  let result = "\n\n-------------\nIN STOCK MOFI\n-------------\n";
+  let result = `\n\n-------------\nIN STOCK MOFI\n-------------\n`;
 
   elements.each((_: number, el: Element) => {
     const content = $(el).text();
@@ -13,5 +16,5 @@ export function mofiParser(elements: Cheerio<Element>, $: CheerioAPI): string {
     }
   });
 
-  return result;
+  return (result += `\n${ENDPOINT}`);
 }
