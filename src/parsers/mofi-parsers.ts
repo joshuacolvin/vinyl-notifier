5a2450ac6c5bf783d616e67c5856dfa02cd45495
Joshua Colvin <joshua@joshuacolvin.net>
2023-01-15 19:37:18 -0500

    refactor parser

--- src/parsers/mofi-parsers.ts
+++ src/parsers/mofi-parsers.ts
@@ -4,8 +4,9 @@ import { MOFI_FILTERS } from "../constants/mofi-constants";
 export function mofiParser(elements: Cheerio<Element>, $: CheerioAPI): string {
   let result = "\n\n-------------\nIN STOCK MOFI\n-------------\n";
 
-  elements.each((i: number, el: Element) => {
-    const title = $(el).text()?.split("-")?.[1]?.trim()?.toLowerCase() ?? "";
+  elements.each((_: number, el: Element) => {
+    const content = $(el).text();
+    const title = content?.split("-")?.[1]?.trim()?.toLowerCase() ?? "";
 
     if (!MOFI_FILTERS.ALBUMS.includes(title)) {
       result += `${title}\n`;
